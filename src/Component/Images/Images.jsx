d7edbe2fff3312c2dfbc7d1bc5a7cc5cd2373451
Nguyendev037 <nguyenndev037@gmail.com>
2024-08-09 16:49:35 +0700

    update-10

---
+++ src/Component/Images/Images.jsx
@@ -0,0 +1,26 @@
+import imageHero1 from "./Hero/imageHero1.png"
+import imageHero2 from "./Hero/imageHero2.png"
+import imageHero3 from "./Hero/imageHero3.png"
+import imageHero4 from "./Hero/imageHero4.jpg"
+import item1 from "./product-images/item-1.webp";
+import item2 from "./product-images/item-2.webp";
+import item3 from "./product-images/item-3.webp";
+import item4 from "./product-images/item-4.webp";
+import item5 from "./product-images/item-5.webp";
+import item6 from "./product-images/item-6.webp";
+import item7 from "./product-images/item-7.webp";
+import item8 from "./product-images/item-8.webp";
+import item9 from "./product-images/item-9.webp";
+
+
+
+
+export const Images = {
+    imageHero: {
+        "imageHero1": imageHero1,
+        "imageHero2": imageHero2,
+        "imageHero3": imageHero3,
+        "imageHero4": imageHero4,
+    }
+};
+
